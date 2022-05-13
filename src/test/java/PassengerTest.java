eb7a2bd7cf27f456da0d480251aeb713f72e55df
CLg003 <clairemjlaing@gmail.com>
2022-05-13 14:39:49 +0100

    Abstract Person class set up. Passenger class set up, all Passenger tests passed.

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("David", 2);
+    }
+
+    @Test
+    public void passengerHasName(){
+        assertEquals("David", passenger.getName());
+    }
+
+    @Test
+    public void passengerHasBags(){
+        assertEquals(2, passenger.getNumberOfBags());
+    }
+
+}
